b15715e3f63d4263c426feddb78a41a13ff3d3c0
Abhijeet Prasad <abhijeetprasad98@gmail.com>
2017-01-30 17:00:54 -0500

    added button api call

--- app.js
+++ app.js
@@ -1,1 +1,33 @@
 "use strict";
+
+//when button is clicked
+$('#searchBtn').click(function(e) {
+    var searchTerm = "";
+
+    e.preventDefault();
+
+    //gets value from input box
+    searchTerm = $('#inputBox').val();
+    console.log(searchTerm);
+
+    //converts value to term to be used in api
+    var apiSearchTerm = searchTerm.replace(" ", "%20");
+
+    console.log(apiSearchTerm);
+
+    var wikiAPI = "https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&srsearch=" + apiSearchTerm + "&utf8=";
+
+    $.getJSON(wikiAPI).done(updateSearch).fail(errMsg);
+});
+
+//if json request fails
+function errMsg(jqxhr, textStatus, err) {
+    console.log("Request Failed: " + textStatus + ", " + err);
+}
+
+//if json request doesn't fail
+function updateSearch(json) {
+    var wikitext = JSON.stringify(json).replace(/"/g, "");
+
+    $('#test').html(wikitext);
+}
